ff944ed8d185532c9f105ac467b40e9bff8af660
foureverhh <foureverhh@gmail.com>
2018-09-17 15:18:21 +0200

    exampel from class

---
+++ SceneAndSingleton180917/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+
+    AsyncOperation async;
+
+    private void Start()
+    {
+        Player.Instance.transform.position = Vector3.zero;
+
+        Debug.Log("Points are: " + Player.Instance.points);
+        Debug.Log("Scene " + SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name == "Scene1")
+        {
+            async = SceneManager.LoadSceneAsync("Scene2");
+            async.allowSceneActivation = false;
+        }
+        else if (SceneManager.GetActiveScene().name == "Scene2")
+        {
+            async = SceneManager.LoadSceneAsync("Scene1");
+            async.allowSceneActivation = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        /*
+        if(SceneManager.GetActiveScene().name == "Scene1")
+        {
+            SceneManager.LoadScene("Scene2");
+        }else if(SceneManager.GetActiveScene().name == "Scene2")
+        {
+            SceneManager.LoadScene("Scene1");
+        }
+        */
+        async.allowSceneActivation = true;
+        Debug.Log("Points are: "+Player.Instance.points);
+
+    }
+}
---
+++ SceneAndSingleton180917/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private static Player _instance;
+
+    public static Player Instance {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Instantiate(Resources.Load<GameObject>("Player").GetComponent<Player>());
+            }
+            return _instance;
+        }
+    }
+
+    public int points = 0;
+
+    private void Awake()
+    {
+       
+        if(_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        } 
+    }
+    private void OnMouseDown()
+    {
+        points++;
+    }
+}
